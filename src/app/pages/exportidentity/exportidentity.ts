e8f84423a9414accce54c90fcde3c5ac4d1f5f08
Chad <chadracelis@yahoo.com>
2021-01-20 12:32:49 -0500

    #jpvtyf - Add styling and other changes to exportidentity

--- src/app/pages/exportidentity/exportidentity.ts
+++ src/app/pages/exportidentity/exportidentity.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
 import { HiveService } from 'src/app/services/hive.service';
 import { IdentityService } from 'src/app/services/identity.service';
 import { PersistenceService } from 'src/app/services/persistence.service';
 import { Clipboard } from '@ionic-native/clipboard/ngx';
+import { ThemeService } from 'src/app/services/theme.service';
 
 declare let appManager: AppManagerPlugin.AppManager;
 declare let titleBarManager: TitleBarPlugin.TitleBarManager;
@@ -14,12 +15,21 @@ declare let titleBarManager: TitleBarPlugin.TitleBarManager;
   styleUrls: ['exportidentity.scss']
 })
 export class ExportIdentityPage {
+
   public mnemonicCopiedToClipboard = false;
   public didString: string = null;
   public mnemonicWords: string = null;
+  public hideMnemonic = true;
+
   private titleBarListener: (icon: TitleBarPlugin.TitleBarIcon) => void = null;
 
-  constructor(private navCtrl: NavController, public identityService: IdentityService, private clipboard: Clipboard) {}
+  constructor(
+    private navCtrl: NavController,
+    public identityService: IdentityService,
+    private toastCtrl: ToastController,
+    private clipboard: Clipboard,
+    public theme: ThemeService
+  ) {}
 
   async ionViewWillEnter() {
     titleBarManager.setNavigationMode(TitleBarPlugin.TitleBarNavigationMode.HOME);
@@ -44,26 +54,38 @@ export class ExportIdentityPage {
 
   ionViewWillLeave() {
     titleBarManager.setIcon(TitleBarPlugin.TitleBarIconSlot.INNER_LEFT, null);
-
     titleBarManager.removeOnItemClickedListener(this.titleBarListener);
     this.titleBarListener = null;
   }
 
   ionViewDidEnter() {
     appManager.setVisible("show");
-    titleBarManager.setTitle("Export identity");
+    titleBarManager.setTitle("Export Identity");
   }
 
   public showMnemonic() {
-    // TODO
+    this.hideMnemonic = !this.hideMnemonic;
   }
 
   public async copyMnemonicToClipboard() {
     await this.clipboard.copy(this.mnemonicWords);
+    this.toast('Copied to clipboard!');
     this.mnemonicCopiedToClipboard = true;
   }
 
   public async copyDIDStringToClipboard() {
     await this.clipboard.copy(this.didString);
+    this.toast('Copied to clipboard!');
+  }
+
+  async toast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      mode: 'ios',
+      color: 'primary',
+      header: msg,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
   }
 }
